b0f0c97022b6fb60cb3b2a192eaa9a8a79877f26
casobri17 <casobri17@gmail.com>
2024-10-15 15:05:37 -0500

    nfijkaefe

--- sketch.js
+++ sketch.js
@@ -1,47 +1,5 @@
-let imgWigg1, randomMike, imgMike, imgPlaceX, imgPlaceY, pressed, imgLakes, imgCreek, imgWigg2;
-
-function preload() {
-  // Load your new images
-  imgWigg1 = loadImage("wigg1.jpg");
-  imgWigg2 = loadImage("wigg2.jpg");
-  imgMike = loadImage("Mike.png");
-  imgLakes = loadImage("lakes.gif");
-  imgCreek = loadImage("creek.gif");
-  pressed = false;
-}
-
 function setup() {
   createCanvas(400, 400);
-  randomMike = round(random());  // Randomize Mike
-  print(randomMike);
-}
-
-function draw() {
-  background(220);
-  
-  // Display "Lakes" GIF
-  image(imgLakes, 0, 0, 100, 100);  // Adjust size as needed
-  
-  // Display "Creek" GIF
-  image(imgCreek, 100, 0, 100, 100);  // Adjust size as needed
-  
-  // Display Wigg1 image when mouse is pressed
-  if (pressed) {
-    image(imgWigg1, imgPlaceX, imgPlaceY, 1268 / 4, 2016 / 4);  // Maintain aspect ratio for Wigg1
-  }
-
-  // Randomize display of Mike or Wigg2
-  if (randomMike) {
-    image(imgMike, 0, height / 2, 1386 / 4, 1018 / 4);  // Maintain aspect ratio for Mike
-  } else {
-    image(imgWigg2, 0, height / 2, 1512 / 4, 2016 / 4);  // Maintain aspect ratio for Wigg2
-  }
-}
-
-function mousePressed() {
-  // Place Wigg1 at the mouse position when clicked
-  imgPlaceX = mouseX;
-  imgPlaceY = mouseY;
-  pressed = true;
-  print("PLEASE");
+  background(100);  // Test: Should display a grey canvas
+  print("Canvas loaded");  // Check if this prints to the console
 }
